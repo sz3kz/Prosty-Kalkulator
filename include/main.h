579f077f74598e0b3c5ab96b4a7b1087d52bed15
sz3kz <139809645+sz3kz@users.noreply.github.com>
2025-11-17 22:09:37 +0100

    Add interractive mode check

--- include/main.h
+++ include/main.h
@@ -2,5 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define INTERRACTIVE_MODE_ARGUMENT_COUNT 2
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include "../include/main.h"
 
-int main(void){
+int main(int argc, char * argv[]){
+	bool is_interractive = false;
+	if (argc != INTERRACTIVE_MODE_ARGUMENT_COUNT){
+		puts("Invalid program invocation!");
+		printf("Usage: %s -i\n", argv[0]);
+		return -1;
+	}
+	if (argc == INTERRACTIVE_MODE_ARGUMENT_COUNT ){
+		if (strcmp(argv[1], "-i") != 0){
+			puts("Invalid switch!");
+			printf("Usage: %s -i\n", argv[0]);
+			return -1;
+		}
+		is_interractive = true;
+	}
 	puts("Hello World!");
 	return 0;
 }
